c17499126f15ac04e06f9be3487a4aa67fa74273
Abdelrahman Abdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2023-08-20 17:17:39 +0300

    Update

--- Commdoctortarek/lib/data/datasource/static/static.dart
+++ Commdoctortarek/lib/data/datasource/static/static.dart
@@ -1,9 +1,17 @@
-
 import 'package:commdoctortarek/core/constant/imgaeasset.dart';
 import 'package:commdoctortarek/data/model/onboardingmodel.dart';
 
-List<OnBoardingModel> onBoardingList=[
-  OnBoardingModel(title:"Welcome",Body:"Communication Patient application to help",Image: ImageAsset.OnBordingImageone),
-  OnBoardingModel(title:"chat",Body:"Communication Patient application to help",Image: ImageAsset.OnBordingImagetwo),
-  OnBoardingModel(title:"about us",Body:"Communication Patient application to help",Image: ImageAsset.OnBordingImageThere),
+List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(
+      title: "Welcome",
+      Body: "Welcome to the Communication Patient application designed to assist you.",
+      Image: ImageAsset.OnBordingImageone),
+  OnBoardingModel(
+      title: "chat",
+      Body: "Engage in conversations seamlessly with our chat feature.",
+      Image: ImageAsset.OnBordingImagetwo),
+  OnBoardingModel(
+      title: "about us",
+      Body: "Learn more about our mission and how we aim to help patients.",
+      Image: ImageAsset.OnBordingImageThere),
 ];
--- Commdoctortarek/lib/view/screen/onboarding.dart
+++ Commdoctortarek/lib/view/screen/onboarding.dart
@@ -1,18 +1,51 @@
-import 'package:commdoctortarek/data/datasource/static/static.dart';
-import 'package:dartz/dartz.dart';
+import 'package:commdoctortarek/data/datasource/static/static.dart' as staticData;
+import 'package:dartz/dartz.dart'; // Note: this package is imported but not used in your provided code.
 import 'package:flutter/material.dart';
 
 class OnBoarding extends StatelessWidget {
-  const OnBoarding({super.key});
+  const OnBoarding({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: PageView.builder(itemCount: onBoardingList.length
-           ,itemBuilder: (context,i)=>Column(children: [
-                      Text(onBoardingList[i].title!)
+      body: SafeArea(
+        child: PageView.builder(
+          itemCount: staticData.onBoardingList.length,
+          itemBuilder: (context, i) {
+            final item = staticData.onBoardingList[i];
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,  // Align items to the center
+              children: [
+                if (item.title != null)
+                  Text(
+                    item.title!,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                SizedBox(height: 80.0),  // Adding a SizedBox
+                if (item.Image != null) Image.asset(item.Image!),
+                SizedBox(height: 80.0),  // Adding a SizedBox
+                Spacer(),  // Pushes everything below to the center
+                if (item.Body != null)
+                  Text(
+                    item.Body!,
+                    textAlign: TextAlign.center, // This will center-align the text
+                    style: TextStyle(
+                      fontSize: 18.0,  // Adjust font size as needed
+                      height: 1.5,     // This adjusts the line spacing. Adjust the value as needed.
+                      color: Colors.black,
+                    ),
+                  ),
 
-       ], )),
+                Spacer(),  // Takes up remaining space
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
